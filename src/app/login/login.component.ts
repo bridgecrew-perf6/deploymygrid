6c44e2408f0ddedf8ab979e1e5e94ebc92a0ed61
kalimuthu-altrocks <kalimuthu.A@altrockstech.com>
2022-02-17 18:38:51 +0530

    second-commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
-
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  
-
-  constructor() { }
+  form !: FormGroup;
+  submitted = false;
+  constructor( private formBuilder : FormBuilder) { }
   
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      userName : ['', Validators.required],
+      password : ['', Validators.required],
+      acceptTerms : [false, Validators.requiredTrue]
+    })
+  }
+  get f(): {[key : string]:AbstractControl}{
+    return this.form.controls;
+  }
+  onSubmit(): void{
+    this.submitted = true;
+    if(this.form.invalid){
+      return;
+    }
+    console.log(JSON.stringify(this.form.value, null, 2));
   }
   
 
